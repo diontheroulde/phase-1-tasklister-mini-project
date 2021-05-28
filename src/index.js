ab10af658ab0e6a95ec55d2015fc74934424fdbc
Dion Theroulde <dionjr105@gmail.com>
2021-05-28 11:49:13 -0400

    Pair DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector("#create-task-form").addEventListener("submit", (event) =>{
+    event.preventDefault()
+  const newTask = document.querySelector("#new-task-description").value 
+  const tasks = document.querySelector("#tasks")
+  const element = document.createElement("li")
+  element.innerText = newTask
+  tasks.append(element)
+  
+  
+  
+  
+  })
+
+})
